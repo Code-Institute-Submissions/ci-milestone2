d06f262cd32d85c2b9861bc0035e818c929ade8f
Andrew Kenworthy <akenworthy87@gmail.com>
2019-05-10 15:05:27 +0000

    Made psuedo code doc. HTML: Added Start button. JS: Added defines for READYSTATEs, changed id property of game button objects btnId to avoid confusion with html element IDs, add needed global vars

--- assets/js/simon.js
+++ assets/js/simon.js
@@ -1,34 +1,49 @@
-/* global $ */  //Stops C9 flagging a warning for '$ not defined'
+/* global $ */          //Stops C9 flagging a warning for '$ not defined'
+// ######### Defines ##########
+const OFF = 0;          // Game hasn't been started yet
+const UNREADY = 1;      // Game has started but not ready for player input, e.g. currently playing pattern
+const READY = 2;        // Game has started and ready for player input, e.g. players turn to repeat pattern
+const OVER = 3;         // Game is finished
 
-// ########## Button Objects ##########
+
+// ########## Global Vars ##########
+var score = 0;
+var hiscore = 0;
+var readyState = OFF;
+var pattern = [];
+var patPos = 0
+
+
+// ########## Game Button Objects ##########
 let btnYellow = {
-    id: 1, 
+    btnId: 1, 
     element: $("#btn-yellow"),
     litClass: "button-yellow-lit",
     chimeGood: "1a.ogg",
     chimeBad: "1b.ogg"
 };
 let btnBlue = {
-    id: 2, 
+    btnId: 2, 
     element: $("#btn-blue"),
     litClass: "button-blue-lit",
     chimeGood: "2a.ogg",
     chimeBad: "2b.ogg"
 };
 let btnGreen = {
-    id: 3, 
+    btnId: 3, 
     element: $("#btn-green"),
     litClass: "button-green-lit",
     chimeGood: "3a.ogg",
     chimeBad: "3b.ogg"
 };
 let btnRed = {
-    id: 4, 
+    btnId: 4, 
     element: $("#btn-red"),
     litClass: "button-red-lit",
     chimeGood: "4a.ogg",
     chimeBad: "4b.ogg"
 };
+
 
 // ########## Binding click events to elements ##########
 
@@ -38,13 +53,15 @@ $(btnGreen.element).click(function(){onClickGameButton(btnGreen)});
 $(btnRed.element).click(function(){onClickGameButton(btnRed)});
 
 
-
-
 // ########## Functions ##########
 // Returns random whole number between min and max params, inclusive of those numbers
 // Taken from w3schools (https://www.w3schools.com/js/js_random.asp)
 function rand(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function onClickStartButton(){
+    
 }
 
 function onClickGameButton(gb){
--- docs/POA.md
+++ docs/POA.md
@@ -2,10 +2,36 @@
 2. Readme
     * ~Outline~
     * UX
-        * User stories
+        * ~User stories~
         * ~Wireframes~
-        * Psuedocode
+        * ~Logic description~
 3. HTML
+    - Basic functions and elements
+        - score
+        - high score
+        - yellow, blue, green, red game buttons
+        - start button
+        - instructions area
+            - make collapsable
+        - history of simon area
+            - make collapsable
 4. CSS
+    - Button super class
+    - button classes
+    - button lit state classes
 5. JS
+    - Global vars and defines
+    - Objects
+    - onClick events
+    - functions
+        - start
+        - nextRound
+        - playPattern
+        - playButton
+        - updateScores
+        - gameButtonClick
 6. Testing doc
+    - In dev notes
+7. ~Psudeocode~
+8. Extended features
+    - Difficulty modes 
